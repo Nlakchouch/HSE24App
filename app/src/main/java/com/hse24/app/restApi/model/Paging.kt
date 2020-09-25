7df7acd0c3c5714f16fdd27b7fc3a85b5bebd8e0
lnawfal <naoufal.ahmed@gmail.com>
2020-09-25 01:10:56 +0100

    Improving user experience when changing screen configuration

--- app/src/main/java/com/hse24/app/restApi/model/Paging.kt
+++ app/src/main/java/com/hse24/app/restApi/model/Paging.kt
@@ -1,14 +1,37 @@
 package com.hse24.app.restApi.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Paging (
     @SerializedName("pageSize")
     var pageSize : Int,
-
     @SerializedName("page")
     var page : Int,
-
     @SerializedName("numPages")
     var numPages : Int
-)
+) : Parcelable {
+
+    constructor(source: Parcel) : this(source.readInt(), source.readInt(), source.readInt())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(pageSize)
+        parcel.writeInt(page)
+        parcel.writeInt(numPages)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Paging> {
+        override fun createFromParcel(parcel: Parcel): Paging {
+            return Paging(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Paging?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
